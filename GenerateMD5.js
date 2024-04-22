12a4ecb9f44dcf0339b055945023b6f7ab059236
Ruofan <flydrf@gmail.com>
2024-04-22 18:07:20 -0400

    change the readFile encoding to base64

--- GenerateMD5.js
+++ GenerateMD5.js
@@ -6,20 +6,18 @@ const dbPath = "./public/coordinatePublic.db";
 // Function to generate MD5 hash for a file
 function generateMD5() {
   return new Promise((resolve, reject) => {
-    const hash = crypto.createHash("md5");
-    const stream = fs.createReadStream(dbPath);
+    // Read the file as a buffer
+    fs.readFile(dbPath, { encoding: "base64" }, (err, fileContents) => {
+      if (err) {
+        reject(err);
+        return;
+      }
 
-    stream.on("data", (data) => {
-      hash.update(data);
-    });
-
-    stream.on("end", () => {
+      // Create MD5 hash
+      const hash = crypto.createHash("md5");
+      hash.update(fileContents); // Update hash with file data buffer
       const md5Hash = hash.digest("hex");
       resolve(md5Hash);
-    });
-
-    stream.on("error", (error) => {
-      reject(error);
     });
   });
 }
