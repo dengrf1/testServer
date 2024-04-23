8c869fbf60636689a4db22c5a428c4fd45748e4b
Ruofan <flydrf@gmail.com>
2024-04-23 04:22:50 -0400

    changed some comments

--- GenerateMD5.js
+++ GenerateMD5.js
@@ -3,19 +3,17 @@ const crypto = require("crypto");
 
 const dbPath = "./public/coordinatePublic.db";
 
-// Function to generate MD5 hash for a file
+// Function to generate MD5 hash for coordinatePublic.db
 function generateMD5() {
     return new Promise((resolve, reject) => {
-        // Read the file as a buffer
         fs.readFile(dbPath, { encoding: "base64" }, (err, fileContents) => {
             if (err) {
                 reject(err);
                 return;
             }
 
-            // Create MD5 hash
             const hash = crypto.createHash("md5");
-            hash.update(fileContents); // Update hash with file data buffer
+            hash.update(fileContents);
             const md5Hash = hash.digest("hex");
             resolve(md5Hash);
         });
